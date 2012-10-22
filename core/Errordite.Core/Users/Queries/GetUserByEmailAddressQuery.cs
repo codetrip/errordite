9ef8c8509024fb812fab6ea6f4bcea08e8a2af3a
Gareth Thackeray <gareththackeray@gmail.com>
2012-10-22 22:03:20 +0100

    authentication using FormsAuthentication

---
+++ core/Errordite.Core/Users/Queries/GetUserByEmailAddressQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CodeTrip.Core;
+using CodeTrip.Core.Interfaces;
+using Errordite.Core.Domain.Organisation;
+using Errordite.Core.Session;
+using System.Linq;
+
+namespace Errordite.Core.Users.Queries
+{
+    public interface IGetUserByEmailAddressQuery : IQuery<GetUserByEmailAddressRequest, GetUserByEmailAddressResponse>
+    {
+    }
+
+    //TODO: caching (maybe)
+    public class GetUserByEmailAddressQuery : SessionAccessBase, IGetUserByEmailAddressQuery
+    {
+        public GetUserByEmailAddressResponse Invoke(GetUserByEmailAddressRequest request)
+        {
+            var user = Query<User>().FirstOrDefault(u => u.Email == request.EmailAddress);
+
+            return new GetUserByEmailAddressResponse()
+                {
+                    User = user,
+                };
+        }
+    }
+
+    public class GetUserByEmailAddressRequest
+    {
+        public string EmailAddress { get; set; }
+    }
+
+    public class GetUserByEmailAddressResponse
+    {
+        public User User { get; set; }
+    }
+
+}
