a711e7082a4479ae1a204abb5de8eb0471df6aa0
Nick Champion <nick.champion@profero.com>
2012-12-19 15:57:38 +0000

    added missing files

---
+++ core/Errordite.Core/Messages/SyncIssueErrorCountsMessage.cs
@@ -0,0 +1,16 @@
+﻿
+using Errordite.Core.ServiceBus;
+
+namespace Errordite.Core.Messages
+{
+    public class SyncIssueErrorCountsMessage : ErrorditeNServiceBusMessageBase
+    {
+        public string IssueId { get; set; }
+        public string OrganisationId { get; set; }
+
+        public SyncIssueErrorCountsMessage()
+        {
+            DoNotAudit = true;
+        }
+    }
+}
---
+++ core/Errordite.Core/Session/DeleteByIndexCommitAction.cs
@@ -0,0 +1,24 @@
+﻿using Raven.Abstractions.Data;
+
+namespace Errordite.Core.Session
+{
+    public class DeleteByIndexCommitAction : SessionCommitAction
+    {
+        private readonly string _indexName;
+        private readonly IndexQuery _query;
+        private readonly bool _allowStale;
+
+        public DeleteByIndexCommitAction(string indexName, IndexQuery query, bool allowStale = false)
+            : base("Delete by index")
+        {
+            _indexName = indexName;
+            _query = query;
+            _allowStale = allowStale;
+        }
+
+        public override void Execute(IAppSession session)
+        {
+            session.Raven.Advanced.DocumentStore.DatabaseCommands.DeleteByIndex(_indexName, _query, _allowStale);
+        }
+    }
+}
