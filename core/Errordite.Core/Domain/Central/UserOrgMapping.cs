3e129b51a3e13a88ce5a43478b70e0314c4b38e4
Nick Champion <nick.champion@profero.com>
2013-01-30 15:56:44 +0000

    REMOVED FILES

--- core/Errordite.Core/Domain/Central/UserOrgMapping.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Errordite.Core.Domain.Central
-{
-    public class UserOrgMapping
-    {
-        public string EmailAddress { get; set; }
-        public string OrganisationId { get; set; }
-    }
-}
--- core/Errordite.Core/Messages/CheckIssueMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Errordite.Core.ServiceBus;
-
-namespace Errordite.Core.Messages
-{
-    public class CheckIssueMessage : ErrorditeNServiceBusMessageBase
-    {
-        public string IssueId { get; set; }
-    }
-}
