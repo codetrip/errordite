8331df3b83b269511ab5415f29b20b7a8d6ed44e
Nick Champion <nick.champion@profero.com>
2013-01-30 15:56:01 +0000

    added missing filews

---
+++ core/Errordite.Core/Extensions/RavenQueryableExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Raven.Client.Linq;
+
+namespace Errordite.Core.Extensions
+{
+    public static class RavenQueryableExtensions
+    {
+        public static IRavenQueryable<T> ConditionalWhere<T>(this IRavenQueryable<T> query, Expression<Func<T, bool>> predicate, Func<bool> condition)
+        {
+            if (condition())
+                query = query.Where(predicate);
+
+            return query;
+        }
+
+        public static IQueryable<T> ConditionalWhere<T>(this IQueryable<T> query, Expression<Func<T, bool>> predicate, Func<bool> condition)
+        {
+            if (condition())
+                query = query.Where(predicate);
+
+            return query;
+        }
+    }
+}
