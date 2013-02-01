b0ec20c804c9a5ae4255984e320aefebce22cdbc
Nick <Nick@localhost>
2013-02-01 07:07:03 +0000

    added hover state to dashboard links

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
-call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
+REM %VS110COMNTOOLS%
+call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
 
 "%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "%CD%\core\Errordite.Build\Projects\Build.proj" /logger:FileLogger,Microsoft.Build.Engine;Package.log /p:Configuration=Debug /p:Targets="Build" /p:Platform="Any Cpu" /p:Branch=trunk /m 
 
