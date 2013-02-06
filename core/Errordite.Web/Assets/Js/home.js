160ccff6c7fc22adadb1bcaaf9768c788436c2fc
Nick Champion <nick.champion@profero.com>
2013-02-06 08:06:21 +0000

    added video to home page

--- core/Errordite.Web/Assets/Js/home.js
+++ core/Errordite.Web/Assets/Js/home.js
@@ -4,11 +4,36 @@
     var $body;
     $body = $('section#home');
     if ($body.length > 0) {
-      return $body.delegate('a.showplayer', 'click', function() {
-        var $preview, $this, $video;
+      $body.delegate('a#showplayer', 'click', function() {
+        var $panel, $player, $preview, $this;
         $this = $(this);
-        $preview = $this.closest('div');
-        $video = $preview.next('div.player');
+        $panel = $this.closest('div.video-panel');
+        $preview = $this.closest('div.preview');
+        $player = $panel.find('div.player');
+        $preview.hide();
+        $player.show();
+        $panel.animate({
+          height: "635px"
+        }, 500, function() {
+          $player.find('iframe').show();
+          return $player.find('div.hide-button').show();
+        });
+        return false;
+      });
+      return $body.delegate('a#hideplayer', 'click', function() {
+        var $panel, $player, $preview, $this;
+        $this = $(this);
+        $panel = $this.closest('div.video-panel');
+        $preview = $panel.find('div.preview');
+        $player = $this.closest('div.player');
+        $player.find('iframe').hide();
+        $player.find('div.hide-button').hide();
+        $player.hide();
+        $panel.animate({
+          height: "135px"
+        }, 500, function() {
+          return $preview.show();
+        });
         return false;
       });
     }
