86b86b0af42d83c67706a080408c0190639f7764
Gareth Thackeray <gareth.thackeray@profero.com>
2013-02-09 11:57:55 +0000

    put quotes in bulid destination parameter

--- generate-package.cmd
+++ generate-package.cmd
@@ -2,7 +2,7 @@
 @cd /d "%~dp0"
 call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
 
-"%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "%CD%\core\Errordite.Build\Projects\Package.proj" /logger:FileLogger,Microsoft.Build.Engine;Package.log /p:Configuration=Release /p:Targets="Package" /p:Platform="Any Cpu" /p:Branch=trunk /p:ErrorditeBuildDestination=%ErrorditeBuildDestination% /m 
+"%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "%CD%\core\Errordite.Build\Projects\Package.proj" /logger:FileLogger,Microsoft.Build.Engine;Package.log /p:Configuration=Release /p:Targets="Package" /p:Platform="Any Cpu" /p:Branch=trunk /p:ErrorditeBuildDestination="%ErrorditeBuildDestination%" /m 
 
 pause
 if NOT %ERRORLEVEL% == 0 pause
