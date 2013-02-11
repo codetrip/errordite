8ba02042d579aa03c8ed0ed15711b2eadfb9fa92
Nick Champion <nick.champion@profero.com>
2013-02-11 20:40:55 +0000

    added missing files

---
+++ core/Errordite.Core/Indexing/History_Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Errordite.Core.Domain.Error;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace Errordite.Core.Indexing
+{
+    public class HistoryDocument
+    {
+        public string UserId { get; set; }
+        public string IssueId { get; set; }
+        public DateTime DateAddedUtc { get; set; }
+    }
+
+    public class History_Search : AbstractIndexCreationTask<IssueHistory, HistoryDocument>
+    {
+        public History_Search()
+        {
+            Map = history => from doc in history
+                            select new
+                            {
+                                doc.IssueId,
+                                doc.UserId,
+                                doc.DateAddedUtc
+                            };
+
+            Sort(e => e.DateAddedUtc, SortOptions.String);
+        }
+    }
+}
