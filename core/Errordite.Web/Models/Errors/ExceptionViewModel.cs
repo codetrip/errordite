4c9dca53593e1c69b9782aa9116dda3bdf1c56fc
Gareth Thackeray <gareth.thackeray@profero.com>
2013-03-31 20:52:54 +0100

    allow matching on custom data. to turn off just return empty list from GetExtraDataKeysForIssueQuery

--- core/Errordite.Web/Models/Errors/ExceptionViewModel.cs
+++ core/Errordite.Web/Models/Errors/ExceptionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using CodeTrip.Core.Extensions;
 using Errordite.Core.Domain.Error;
 
@@ -7,18 +8,23 @@ namespace Errordite.Web.Models.Errors
     public class ExceptionViewModel
     {
         public ExceptionInfo Info { get; set; }
+
+        public List<ExtraDataItemViewModel> ExtraData { get; set; }
         public string Url { get; set; }
         public string UserAgent { get; set; }
         public bool InnerException { get; set; }
         public string MachineName { get; set; }
 
-        public ExceptionViewModel(ExceptionInfo info, string url, string userAgent, string machineName, bool innerException = false)
+        public ExceptionViewModel(ExceptionInfo info, string url, string userAgent, string machineName,
+            bool innerException,
+            List<ExtraDataItemViewModel> extraData)
         {
             Info = info;
             Url = url;
             UserAgent = userAgent;
             InnerException = innerException;
             MachineName = machineName;
+            ExtraData = extraData;
         }
 
         
@@ -31,5 +37,12 @@ namespace Errordite.Web.Models.Errors
 
 			return Url.IsNotNullOrEmpty() || UserAgent.IsNotNullOrEmpty() || Info.MethodName.IsNotNullOrEmpty() || Info.Module.IsNotNullOrEmpty() || (Info.ExtraData != null && Info.ExtraData.Count > 0);
 		}
+    }
+
+    public class ExtraDataItemViewModel
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public bool CanMakeRule { get; set; }
     }
 }
