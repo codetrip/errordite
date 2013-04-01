87768435280feef8f37c1c1a7a1b81e69529ec09
Gareth Thackeray <gareth.thackeray@profero.com>
2013-04-01 11:31:19 +0100

    updated client from nuget and added some temp debugging

--- core/Errordite.Web/Models/Errors/ErrorCriteriaViewModel.cs
+++ core/Errordite.Web/Models/Errors/ErrorCriteriaViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using CodeTrip.Core.Paging;
+using Errordite.Client;
 using Errordite.Core.Domain.Error;
 using App = Errordite.Core.Domain.Organisation;
 using System.Linq;
@@ -34,21 +35,34 @@ namespace Errordite.Web.Models.Errors
         public bool HideIssues { get; set; }
 		public string ApplicationName { get; set; }
         public IEnumerable<ExceptionViewModel> Exceptions {get
-        {
-            return Error.ExceptionInfos
-                        .Select((ei, i) =>
-                                new ExceptionViewModel(ei, 
-                                    i == 0 ? Error.Url : null, 
-                                    i == 0 ? Error.UserAgent : null, 
-                                    i == 0 ? Error.MachineName : null, 
-                                    i > 0, 
-                                    ei.ExtraData.Select(kvp => new ExtraDataItemViewModel()
+        {   
+                return Error.ExceptionInfos
+                            .Select((ei, i) =>
+                                {
+                                    using (new ErrorditeScope(() => new Dictionary<string, string>
                                         {
-                                            Key = kvp.Key,
-                                            Value = kvp.Value,
-                                            CanMakeRule = PropertiesEligibleForRules.Contains(kvp.Key),
-                                        }).ToList()
-                                    ));
+                                            {"Temp.Exception", Error.Id},
+                                            {"Temp.InnerIndex", i.ToString()},
+                                        }))
+                                    {
+                                        return new ExceptionViewModel(ei,
+                                                                      i == 0 ? Error.Url : null,
+                                                                      i == 0 ? Error.UserAgent : null,
+                                                                      i == 0 ? Error.MachineName : null,
+                                                                      i > 0,
+                                                                      (ei.ExtraData ?? new Dictionary<string, string>())
+                                                                          .Select(
+                                                                              kvp => new ExtraDataItemViewModel()
+                                                                                  {
+                                                                                      Key = kvp.Key,
+                                                                                      Value = kvp.Value,
+                                                                                      CanMakeRule =
+                                                                                          PropertiesEligibleForRules
+                                                                                         .Contains(kvp.Key),
+                                                                                  }).ToList()
+                                            );
+                                    }
+                                });
         }}
 
         public List<string> PropertiesEligibleForRules { get; set; }
