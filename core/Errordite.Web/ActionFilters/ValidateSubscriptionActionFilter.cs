5dc7557166d1dd80cd5fa42e68c79490d3c1f83b
Nick <Nick@localhost>
2013-04-18 06:08:15 +0100

    started on signup to chargify page and action filter to prevent access once subscriptions have expired

---
+++ core/Errordite.Web/ActionFilters/ValidateSubscriptionActionFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using Errordite.Core.Identity;
+using Errordite.Web.Controllers;
+
+namespace Errordite.Web.ActionFilters
+{
+    public class ValidateSubscriptionActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controller = filterContext.Controller as ErrorditeController;
+
+            if (controller == null)
+                return;
+
+            var appContext = controller.Core.AppContext;
+
+            if(appContext.AuthenticationStatus == AuthenticationStatus.Authenticated && 
+               appContext.CurrentUser.Organisation.PaymentPlan.IsTrial &&
+               appContext.CurrentUser.Organisation.CreatedOnUtc > DateTime.UtcNow.AddDays(-30))
+            {
+                
+            }
+        }
+    }
+}
---
+++ core/Errordite.Web/Controllers/SubscriptionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Errordite.Core.Domain.Organisation;
+using Errordite.Web.Models.Subscription;
+
+namespace Errordite.Web.Controllers
+{
+    public class SubscriptionController : ErrorditeController
+    {
+        [HttpGet, Authorize]
+        public ActionResult SignUp()
+        {
+            var model = new SignUpViewModel
+            {
+                Countries = new List<SelectListItem>(),
+                CreditCards = new List<SelectListItem>(),
+                Plans = new List<PaymentPlan>()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost, Authorize]
+        public ActionResult SignUp(SignUpViewModel model)
+        {
+            return View();
+        }
+    }
+}
