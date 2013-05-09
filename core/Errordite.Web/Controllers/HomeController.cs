265a3d497604504d5998d7bfce8e81b7879883dd
Nick Champion <nick.champion@profero.com>
2013-05-09 07:18:02 +0100

    remove temp stuff from home page

--- core/Errordite.Web/Controllers/HomeController.cs
+++ core/Errordite.Web/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
+﻿using System.ComponentModel.Composition.Hosting;
 using System.Web.Mvc;
 using Errordite.Core;
 using Errordite.Core.Caching.Interfaces;
 using Errordite.Core.Caching.Resources;
-using Errordite.Core.Domain.Error;
 using Errordite.Core.Domain.Master;
 using Errordite.Core.Domain.Organisation;
 using Errordite.Core.Indexing;
@@ -20,9 +17,7 @@ using Errordite.Web.ActionFilters;
 using Errordite.Web.Models.Home;
 using Errordite.Core.Extensions;
 using Errordite.Web.Extensions;
-using Raven.Client;
 using Raven.Client.Indexes;
-using System.Linq;
 
 namespace Errordite.Web.Controllers
 {
@@ -39,144 +34,6 @@ namespace Errordite.Web.Controllers
 	        _session = session;
         }
 
-		[HttpGet, ImportViewData]
-		public ActionResult SetOrg()
-		{
-			var session = ObjectFactory.GetObject<IAppSession>();
-
-			var ravenInstances = session.MasterRaven.Query<RavenInstance>().ToList();
-
-			foreach (var organisation in session.MasterRaven.Query<Organisation, Organisations>())
-			{
-				organisation.RavenInstance = ravenInstances.First(r => r.Id == organisation.RavenInstanceId);
-
-				using (_session.SwitchOrg(organisation))
-				{
-					foreach (var user in _session.Raven.Query<User, Users>())
-					{
-						user.OrganisationId = organisation.Id;
-					}
-				}
-			}
-
-			session.Commit();
-			return Content("Done");
-		}
-
-		[HttpGet, ImportViewData]
-		public ActionResult Users()
-		{
-			var session = ObjectFactory.GetObject<IAppSession>();
-
-			var ravenInstances = session.MasterRaven.Query<RavenInstance>().ToList();
-
-			foreach (var organisation in session.MasterRaven.Query<Organisation, Organisations>())
-			{
-				organisation.RavenInstance = ravenInstances.First(r => r.Id == organisation.RavenInstanceId);
-
-				using (_session.SwitchOrg(organisation))
-				{
-					foreach (var user in _session.Raven.Query<User, Users>())
-					{
-						_session.MasterRaven.Store(new UserOrganisationMapping
-						{
-							EmailAddress = user.Email,
-							Organisations = new List<string> { organisation.Id}
-						});
-					}
-				}
-			}
-
-			session.Commit();
-			return Content("Done");
-		}
-
-		[HttpGet, ImportViewData]
-		public ActionResult CountsFix()
-		{
-			Server.ScriptTimeout = 7200; //timeout in 2 hours
-
-			var session = ObjectFactory.GetObject<IAppSession>();
-
-			var ravenInstances = session.MasterRaven.Query<RavenInstance>().ToList();
-
-			foreach (var organisation in session.MasterRaven.Query<Organisation, Organisations>())
-			{
-				organisation.RavenInstance = ravenInstances.First(r => r.Id == organisation.RavenInstanceId);
-
-				using (_session.SwitchOrg(organisation))
-				{
-					RavenQueryStatistics stats;
-
-					foreach (var issue in _session.Raven.Query<Issue, Issues>().Statistics(out stats)
-						.Skip(0)
-						.Take(25)
-						.As<Issue>()
-						.ToList())
-					{
-					    var count = _session.Raven.Load<IssueHourlyCount>("IssueHourlyCount/{0}".FormatWith(issue.FriendlyId));
-                        if (count == null)
-                        {
-                            var issueHourlyCount = new IssueHourlyCount
-                            {
-                                IssueId = issue.Id,
-                                Id = "IssueHourlyCount/{0}".FormatWith(issue.FriendlyId),
-                                ApplicationId = issue.ApplicationId
-                            };
-
-                            issueHourlyCount.Initialise();
-                            _session.Raven.Store(issueHourlyCount);
-                        }
-                        else
-                        {
-                            count.ApplicationId = issue.ApplicationId;
-                        }
-					}
-
-					_session.Commit();
-					_session.Close();
-
-					if (stats.TotalResults > 25)
-					{
-						int pageNumber = stats.TotalResults / 25;
-
-						for (int i = 1; i < pageNumber; i++)
-						{
-                            foreach (var issue in _session.Raven.Query<Issue, Issues>()
-								.Skip(i * 25)
-								.Take(25)
-								.As<Issue>())
-							{
-                                var count = _session.Raven.Load<IssueHourlyCount>("IssueHourlyCount/{0}".FormatWith(issue.FriendlyId));
-                                if (count == null)
-                                {
-                                    var issueHourlyCount = new IssueHourlyCount
-                                    {
-                                        IssueId = issue.Id,
-                                        Id = "IssueHourlyCount/{0}".FormatWith(issue.FriendlyId),
-                                        ApplicationId = issue.ApplicationId
-                                    };
-
-                                    issueHourlyCount.Initialise();
-                                    _session.Raven.Store(issueHourlyCount);
-                                }
-                                else
-                                {
-                                    count.ApplicationId = issue.ApplicationId;
-                                }
-							}
-
-							_session.Commit();
-							_session.Close();
-						}
-					}
-				}
-			}
-
-			session.Commit();
-			return Content("Done");
-		}
-
 		[HttpGet, ExportViewData]
 		public ActionResult SyncIndexes()
 		{
@@ -189,17 +46,17 @@ namespace Errordite.Web.Controllers
 				new AssemblyCatalog(typeof(Issues).Assembly), new ExportProvider[0]),
 				masterDocumentStore.DatabaseCommands.ForDatabase(CoreConstants.ErrorditeMasterDatabaseName),
 				masterDocumentStore.Conventions);
-
 			Trace("Done Syncing Errordite Indexes");
+
 			foreach (var organisation in Core.Session.MasterRaven.Query<Organisation>().GetAllItemsAsList(100))
 			{
 				organisation.RavenInstance = Core.Session.MasterRaven.Load<RavenInstance>(organisation.RavenInstanceId);
 
 				using (_session.SwitchOrg(organisation))
 				{
-					Trace("Done Syncing {0} Indexes", organisation.Name);
-					_session.BootstrapOrganisation(organisation);
 					Trace("Syncing {0} Indexes", organisation.Name);
+					_session.BootstrapOrganisation(organisation);
+					Trace("Done Syncing {0} Indexes", organisation.Name);
 				}
 			}
 
