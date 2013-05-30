b5040081542ed62fe857690d9aa1055fd1d9e42a
Nick <Nick@localhost>
2013-05-30 06:57:11 +0100

    completed work on organisation stats, removed some caching UI stuff and changed the search to take you directly to a matching issue if found

--- core/Errordite.Core/Indexing/Issues.cs
+++ core/Errordite.Core/Indexing/Issues.cs
@@ -40,7 +40,7 @@ namespace Errordite.Core.Indexing
                                 doc.ErrorCount,
                                 doc.RulesHash,
                                 FriendlyId = int.Parse(doc.Id.Split('/')[1]),
-                                Query = new[] { doc.Name }.Union(doc.Rules.Select(r => r.SearchString)),  
+                                Query = new[] { doc.Name, doc.FriendlyId }.Union(doc.Rules.Select(r => r.SearchString)),  
                             };
 
             Indexes = new Dictionary<Expression<Func<IssueDocument, object>>, FieldIndexing>
