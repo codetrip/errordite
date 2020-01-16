1e8bc744c86cfdcc584cd3cbf89ba5596bf8564c
Nick Champion <nick.champion@reebonz.com>
2020-01-16 09:43:24 +0000

    Log email message on error + fixed project references

--- core/Errordite.Core/Notifications/Sending/SmtpEmailSender.cs
+++ core/Errordite.Core/Notifications/Sending/SmtpEmailSender.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Mail;
 using Errordite.Core.Extensions;
 using Errordite.Core.Configuration;
+using Newtonsoft.Json;
 
 namespace Errordite.Core.Notifications.Sending
 {
@@ -23,60 +24,69 @@ namespace Errordite.Core.Notifications.Sending
             Trace("Starting...");
             Trace("Sending email message to:={0}...", message.To);
 
-            using (var m = new MailMessage())
+            try
             {
-                m.From = new MailAddress(_config.FromAddress, _config.FromDisplayName);
-                m.Body = message.Body;
-                m.Subject = message.Subject;
-                m.IsBodyHtml = true;
-
-                if (message.To.IsNotNullOrEmpty())
+                using (var m = new MailMessage())
                 {
-                    foreach (string mail in message.To.Split(new [] { ',', ';' }))
-                    {
-                        m.To.Add(new MailAddress(mail));
-                    }
-                }
+                    m.From = new MailAddress(_config.FromAddress, _config.FromDisplayName);
+                    m.Body = message.Body;
+                    m.Subject = message.Subject;
+                    m.IsBodyHtml = true;
 
-                if (!message.Cc.IsNullOrEmpty())
-                {
-                    foreach (string mail in message.Cc.Split(new [] { ',', ';' }))
+                    if (message.To.IsNotNullOrEmpty())
                     {
-                        m.CC.Add(new MailAddress(mail));
+                        foreach (string mail in message.To.Split(new[] { ',', ';' }))
+                        {
+                            m.To.Add(new MailAddress(mail));
+                        }
                     }
-                }
 
-                if (!message.Bcc.IsNullOrEmpty())
-                {
-                    foreach (string mail in message.Bcc.Split(new [] { ',', ';' }))
+                    if (!message.Cc.IsNullOrEmpty())
                     {
-                        m.Bcc.Add(new MailAddress(mail));
+                        foreach (string mail in message.Cc.Split(new[] { ',', ';' }))
+                        {
+                            m.CC.Add(new MailAddress(mail));
+                        }
                     }
-                }
 
-                if (!message.ReplyTo.IsNullOrEmpty())
-                {
-                    foreach (string mail in message.ReplyTo.Split(new [] { ',', ';' }))
+                    if (!message.Bcc.IsNullOrEmpty())
                     {
-                        m.ReplyToList.Add(mail);
+                        foreach (string mail in message.Bcc.Split(new[] { ',', ';' }))
+                        {
+                            m.Bcc.Add(new MailAddress(mail));
+                        }
                     }
-                }
 
-                using (var client = new SmtpClient(_config.SmtpServer, _config.SmtpServerPort)
+                    if (!message.ReplyTo.IsNullOrEmpty())
+                    {
+                        foreach (string mail in message.ReplyTo.Split(new[] { ',', ';' }))
+                        {
+                            m.ReplyToList.Add(mail);
+                        }
+                    }
+
+                    using (var client = new SmtpClient(_config.SmtpServer, _config.SmtpServerPort)
                     {
                         EnableSsl = _config.IsSmtpSecureConnection
                     })
-                {
-                    if (!String.IsNullOrEmpty(_config.SmtpServerUsername))
                     {
-                        client.Credentials = new NetworkCredential(_config.SmtpServerUsername, _config.SmtpServerPassword);
-                    }
+                        if (!String.IsNullOrEmpty(_config.SmtpServerUsername))
+                        {
+                            client.Credentials = new NetworkCredential(_config.SmtpServerUsername, _config.SmtpServerPassword);
+                        }
 
-                    Trace("Sending...");
-                    client.Send(m);
-                    watch.Stop();
-                    Trace("...Sent, Elapsed:={0} ({1}ms)", watch.Elapsed, watch.ElapsedMilliseconds);
+                        Trace("Sending...");
+                        client.Send(m);
+                        watch.Stop();
+                        Trace("...Sent, Elapsed:={0} ({1}ms)", watch.Elapsed, watch.ElapsedMilliseconds);
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                e.Data.Add("Message", JsonConvert.SerializeObject(message));
+                Error(e);
+                throw;
             }
         }
     }
